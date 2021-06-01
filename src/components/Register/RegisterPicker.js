aa2ae01e5c89acd9d909c0ce560a566e3f7fb515
Jessi19Jassi <jessyhi44@hotmail.com>
2021-06-01 00:20:52 -0500

    Mapa

--- src/components/Register/RegisterPicker.js
+++ src/components/Register/RegisterPicker.js
@@ -4,7 +4,6 @@ import './RegisterPicker.css'
 import swal from 'sweetalert';
 import { useHistory } from 'react-router-dom'
 
-
 export const RegisterPicker = (props) => {
 
     let history = useHistory();
@@ -12,6 +11,7 @@ export const RegisterPicker = (props) => {
     const[name, setName] = useState('');
     const[ine, setIne] = useState('');
     const[placas, setPlacas] = useState('');
+    const[transport, setTransport] = useState('');
     const[phone, setPhone] = useState('');
     // console.log(props.user.uid);
     
@@ -21,6 +21,7 @@ export const RegisterPicker = (props) => {
             name : name,
             ine: ine,
             placas: placas,
+            transport: transport,
             phone: phone, 
         };
         try{
@@ -46,16 +47,16 @@ export const RegisterPicker = (props) => {
                         <input value={ine} onChange={(e) => {setIne(e.target.value)}} type="text" className="registerPickerInput" />
                         <label htmlFor="key">No. de placas*</label>
                         <input value={placas} onChange={(e) => {setPlacas(e.target.value)}} type="text" className="registerPickerInput" />
-                        <div className="radioButtom">
-                            <div className="radioButtomOne">
-                                <input type="radio" name="bicicleta"/>Bicicleta
-                                <input type="radio" name="automovil"/>Automóvil
-                            </div>
-                            <div className="radioButtomTwo">
-                                <input type="radio" name="motocicleta"/>Motocicleta
-                                <input type="radio" name="caminando"/>Caminando
-                            </div>
-                        </div>
+                        <select className="radioButtom" value={transport} onChange={(e) => {setTransport(e.target.value)}} required>
+                            {/* <div className="radioButtomOne"> */}
+                                <option  type="radio" name="bicicleta">Bicicleta</option>
+                                <option type="radio" name="automovil" >Automóvil</option>
+                            {/* </div>j
+                            <div className="radioButtomTwo"> */}
+                                <option  type="radio" name="motocicleta">Motocicleta</option>
+                                <option type="radio" name="caminando">Caminando</option>
+                            {/* </div> */}
+                        </select>
                         <label htmlFor="telephone">Teléfono*</label>
                         <input value={phone} onChange={(e) => {setPhone(e.target.value)}} type="number" className="registerPickerInput" />
                         <button onClick={registerPick} value="regPicker" className="registerPickerButton" >UNIRME</button>
